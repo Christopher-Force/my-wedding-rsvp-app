cbc31f0de04f7e42587e4da5ab3a09afe9f478f5
Christopher-Force <chrisforceproduction@outlook.com>
2022-08-18 00:09:37 -0400

    added subdomain wedding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  if (window.location.host.split(".")[0] === "wedding") {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            This subdomain is working properly.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      // <Router>
+      //   <div className="bg-black">
+      //     <Wrapper>
+      //       <div>
+      //         <LandingNav />
+      //       </div>
+      //       <Routes>
+      //         <Route path="/" element={<RoofLanding />} />
+      //         <Route path="thankyou" element={<ThankYou />} />
+      //       </Routes>
+      //       {/* HIDDEN POPUP */}
+      //       <div>
+      //         <input type="checkbox" id="roof-popup" className="modal-toggle" />
+      //         <label className="modal items-start lg:items-center pt-4 md:pt-8 " for="roof-popup">
+      //           <label className="modal-box w-11/12 md:max-w-5xl pt-4 max-h-screen" for="">
+      //             <PopupContact />
+      //           </label>
+      //         </label>
+      //       </div>
+      //     </Wrapper>
+      //   </div>
+      // </Router>
+    )
+  }
+  else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Subdomain is not working properly.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
