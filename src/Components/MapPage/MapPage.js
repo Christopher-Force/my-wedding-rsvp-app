6da0a9a7f8de947215479ff3f57e87067d07abff
Christopher-Force <chrisforceproduction@outlook.com>
2022-08-25 16:20:43 -0400

    Added fonts and formatting

--- src/Components/MapPage/MapPage.js
+++ src/Components/MapPage/MapPage.js
@@ -6,14 +6,32 @@ import RSVPButton from "../Buttons/RSVPButton/RSVPButton";
 export default function MapPage () {
   return (
     <div>
-      Ceremony:
+      <strong>
+        <h1>Map Information</h1>
+      </strong>
+      <p>
+        Ceremony @ 1:00PM
+        <br />
+        PAWLEY'S ISLAND BEACH
+        <br />
+        1st Street Public Beach Access
+        <br />
+        County Rd S-22-10
+        <br />
+        Pawleys Island, SC 29585
+      </p>
       <br />
-      Time:
-      <br />
-      <br />
-      Reception:
-      <br />
-      Time:
+      <p>
+        Reception To Follow 
+        <br />
+        Starting @ 2:00-2:30PM Until 6:00-6:30PM
+        <br />
+        Family-Owned Rental Home - Backyard
+        <br />
+        43 Windy Lane
+        <br />
+        Pawleys Island, SC 29585
+      </p>
       <br />
       <br />
       <h2>Route from Ceremony to Reception</h2>
