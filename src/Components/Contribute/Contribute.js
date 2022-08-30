359392d4f42ebe42d2208c58e07ac3de83b2f943
Christopher-Force <chrisforceproduction@outlook.com>
2022-08-30 14:49:04 -0400

    added on extra

--- src/Components/Contribute/Contribute.js
+++ src/Components/Contribute/Contribute.js
@@ -1,22 +1,55 @@
-import ContributeButton from "../Buttons/ContributeButton/ContributeButton";
 import DetailsButton from "../Buttons/DetailsButton/Detailsbutton";
 import HomeButton from "../Buttons/HomeButton/HomeButton";
 import MapButton from "../Buttons/MapButton/MapButton";
 import RSVPButton from "../Buttons/RSVPButton/RSVPButton";
+import '../Buttons/ChipButton/ChipButton.css';
 
-export default function Contribute () {
+export default function Contribute() {
   return (
     <div>
+      <div>
+          <div>
+            <h1 className="date">
+              11.12.22
+            </h1>
+          </div>
+          <br/>
+          <div>
+            <h2 className="names text-5xl">Christopher and Precious</h2>
+          </div>
+          <br/>
+          <div>
+            <strong>
+              <h3 className="headline text-xl">Celebrate Our Special Day With Us!</h3>
+            </strong>
+          </div>
+          <br />
+          <div>
+            <h3 className="headline u-line text-2xl"><strong>Please RSVP by 09.15.22</strong></h3>
+          </div>
+        </div>
+        <br/>
+      <div>
+        <h1>Amazon Registry:</h1>
+      </div>
+      <br/>
+      <div className="flex flex-col gap-2 mb-8">
+        <h1>Contribute To Our Honeymoon Fund:</h1>
+        <a href="https://buy.stripe.com/7sI7waduj45IeMU6or" class="chipButton w-full">Chip in $10</a>
+        <a href="https://buy.stripe.com/bIY8Aedujau69sA3cc" class="chipButton w-full">Chip in $25</a>
+        <a href="https://buy.stripe.com/3csg2GbmbcCefQY8wy" class="chipButton w-full">Chip in $50</a>
+        <a href="https://buy.stripe.com/9AQeYC2PF31E0W49AB" class="chipButton w-full">Chip in Custom Amount</a>
+      </div>
       <div className="flex gap-2">
-          <RSVPButton />
-          <MapButton />
-        </div>
-        <br />
-        <div className='flex gap-2'>
-        
-          <HomeButton />
-          <DetailsButton />
-        </div>
+        <RSVPButton />
+        <MapButton />
+      </div>
+      <br />
+      <div className='flex gap-2'>
+
+        <HomeButton />
+        <DetailsButton />
+      </div>
     </div>
   )
 }
--- src/Components/MapPage/MapPage.js
+++ src/Components/MapPage/MapPage.js
@@ -1,4 +1,3 @@
-import BackButton from "../Buttons/BackButton/BackButton";
 import ContributeButton from "../Buttons/ContributeButton/ContributeButton";
 import DetailsButton from "../Buttons/DetailsButton/Detailsbutton";
 import HomeButton from "../Buttons/HomeButton/HomeButton";
@@ -7,6 +6,28 @@ import RSVPButton from "../Buttons/RSVPButton/RSVPButton";
 export default function MapPage () {
   return (
     <div>
+      <div>
+          <div>
+            <h1 className="date">
+              11.12.22
+            </h1>
+          </div>
+          <br/>
+          <div>
+            <h2 className="names text-5xl">Christopher and Precious</h2>
+          </div>
+          <br/>
+          <div>
+            <strong>
+              <h3 className="headline text-xl">Celebrate Our Special Day With Us!</h3>
+            </strong>
+          </div>
+          <br />
+          <div>
+            <h3 className="headline u-line text-2xl"><strong>Please RSVP by 09.15.22</strong></h3>
+          </div>
+          <br />
+        </div>
       <strong>
         <h1 className="mb-1">Map Information</h1>
       </strong>
